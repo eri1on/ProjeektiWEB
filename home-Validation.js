809f4037ee7153190fa9328b3abf6025cda0a167
eri1on <et58394@ubt-uni.net>
2023-02-12 15:37:48 +0100

    added a validation function with regex for home.html fields

---
+++ home-Validation.js
@@ -0,0 +1,53 @@
+
+function  Validation(){
+
+const NameRegex=/^[A-Za-z]+[a-zA-Z]{1,}/;
+const LastNameRegex=/^[A-Za-z]+[a-zA-Z]{1,}/;
+const EmailRegex=/^[a-z A-Z 0-9]+\w[_.-]?\w+@[a-z]+.[a-z]{2,3}/;
+
+var NameInput=document.getElementById('name').value;
+var LastnameInput=document.getElementById('lastname').value;
+var EmailInput=document.getElementById('email').value;
+
+
+if(NameInput===""){
+    alert("Please enter your name !")
+    document.getElementById('name').focus();
+    return false;
+}
+
+if(!NameRegex.test(NameInput)){
+    alert("Name should only include letters, and have at least 2 characters");
+    document.getElementById('name').focus();
+    return false;
+}
+
+if(LastnameInput===""){
+  alert("Please enter you lastname");
+  document.getElementById('lastname').focus();
+  return false;
+}
+
+if(!LastNameRegex.test(LastnameInput)){
+     alert("Lastname should only include letters, and have at least 2 characters");
+     document.getElementById('lastname').focus()
+     return false;
+}
+
+
+if(EmailInput===""){
+  alert("Email cannot be empty !");
+  document.getElementById("email").focus();
+  return false;
+}
+
+if(!EmailRegex.test(EmailInput)){
+alert("Please enter a valid email address");
+document.getElementById('email').focus();
+return false;
+}
+   
+
+alert("validation completed successfully");
+
+}
