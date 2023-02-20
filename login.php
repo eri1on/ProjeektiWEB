1922b5a76cc82950b77027a1239745d761be222a
eri1on <et58394@ubt-uni.net>
2023-02-20 03:21:24 +0100

    created login.php file (page)

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="login-style.css">
+    </head>
+    <body>
+    <div class="register">
+<form class="SignUpForm" method="POST">
+  <h2 class="h2">Login</h2>
+  <input id="Name" type="text" name="username" placeholder="Username"><br>
+
+  <input id="email" class="register-email" name="email" type ="email" placeholder="Email">
+  <input  id="password" class="register-password" name="password" type="password"placeholder="Password"><br>
+
+  <input class="submit" type="submit" onclick=ValidimiFormes()>
+</form>
+
+</div>       
+        <script src="" async defer></script>
+    </body>
+</html>
