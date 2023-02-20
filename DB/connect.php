946e16ab396889499f8417aac958b927d94ebe05
eri1on <et58394@ubt-uni.net>
2023-02-20 03:24:57 +0100

    deleted some comments

--- DB/connect.php
+++ DB/connect.php
@@ -8,15 +8,6 @@ $DATABASE='projekti';
 
 $con=mysqli_connect($HOSTNAME,$USERNAME,$PASSWORD,$DATABASE);
 
-/*
-if($con){
-    echo'connection successful.';
-}else{
-    die(mysqli_error($con));
-}
-*/
-
-
 if(!$con){
     die(mysqli_error($con));
 }
