bba9e3d9659a82f249e43ef3d116d2582851bafd
eri1on <et58394@ubt-uni.net>
2023-02-20 16:17:08 +0100

    fixed validation for login page(form)

--- login.js
+++ login.js
@@ -1,8 +1,6 @@
 function ValidimiFormes(){
   
 
-    const NameRegex = /^[A-Za-z]+[a-zA-Z]{1,}/;
-    const PasswordRegex = /^[A-Z a-z 0-9 ?!/.]+\w[A-Z a-z 0-9 ?!/.]/;
     var NameInput = document.getElementById('Name').value;
     var PasswordInput = document.getElementById('password').value;
   
@@ -12,12 +10,6 @@ function ValidimiFormes(){
       return false;
     }
   
-    if (!NameRegex.test(NameInput)) {
-      alert("username can contain only letters and have at least 2 characters !");
-      document.getElementById("Name").focus();
-      return false;
-    }
- 
   
     if (PasswordInput === "") {
       alert("Password cannot be empty !");
@@ -25,19 +17,10 @@ function ValidimiFormes(){
       return false;
     }
   
-    if (!PasswordRegex.test(PasswordInput)) {
-      alert("Please enter a valid Password !!");
-      document.getElementById("password").focus();
-      return false
-    }
+   
   
-    if (PasswordInput.length < 8) {
-      alert("Password must be at least 8 characters long");
-      document.getElementById("password").focus();
-      return false;
-    }
   
-    alert("validation completed successfully");
+ 
     return true;
   }
   
