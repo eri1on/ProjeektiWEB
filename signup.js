0116784e78af73cf45351a3ba9784a4b617ae7f0
eri1on <et58394@ubt-uni.net>
2023-02-21 14:28:08 +0100

    changed  'alert' with .innerText

--- signup.js
+++ signup.js
@@ -3,50 +3,53 @@ function ValidimiFormes() {
 
   const NameRegex = /^[A-Za-z]+[a-zA-Z]{1,}/;
   const EmailRegex = /^[a-z0-9]+([_.-][a-z0-9]+)*@[a-z0-9]+([.-][a-z0-9]+)*\.[a-z]{2,3}$/;
-  const PasswordRegex = /^[A-Z a-z 0-9 ?!/.]+\w[A-Z a-z 0-9 ?!/.]/;
+  const PasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
+  ;
 
   var NameInput = document.getElementById('Name').value;
   var EmailInput = document.getElementById('email').value;
   var PasswordInput = document.getElementById('password').value;
 
+  var errorDiv=document.getElementById('error');
+
   if (NameInput === "") {
-    alert("Please enter your username !");
+    errorDiv.innerText="Please enter your username !";
     document.getElementById("Name").focus();
     return false;
   }
 
   if (!NameRegex.test(NameInput)) {
-    alert("username can contain only letters and have at least 2 characters !");
+   errorDiv.innerText="username can contain only letters and have at least 2 characters !";
     document.getElementById("Name").focus();
     return false;
   }
 
   if (EmailInput === "") {
-    alert("Email cannot be empty");
+    errorDiv.innerText="Email cannot be empty";
     document.getElementById("email").focus();
     return false;
   }
 
   if (!EmailRegex.test(EmailInput)) {
-    alert("Please enter a valid email address");
+    errorDiv.innerText="Please enter a valid email address";
     document.getElementById("email").focus();
     return false;
   }
 
   if (PasswordInput === "") {
-    alert("Password cannot be empty !");
+    errorDiv.innerText="Password cannot be empty !";
     document.getElementById("password").focus();
     return false;
   }
 
   if (!PasswordRegex.test(PasswordInput)) {
-    alert("Please enter a valid Password !!");
+    errorDiv.innerText="Password must be at least 8 characters long and contain at least one uppercase letter and one digit.";
     document.getElementById("password").focus();
     return false
   }
 
   if (PasswordInput.length < 8) {
-    alert("Password must be at least 8 characters long");
+    errorDiv.innerText="Password must be at least 8 characters long";
     document.getElementById("password").focus();
     return false;
   }
