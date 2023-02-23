1c9f0e6f99ebc3b1254e83de53a86c903df9e976
eri1on <et58394@ubt-uni.net>
2023-02-23 12:06:11 +0100

    query for delete course

---
+++ Course/delete-course.php
@@ -0,0 +1,23 @@
+<?php
+
+include '../DB/connect.php';
+
+if (isset($_GET['deleteid'])){
+    $id=$_GET['deleteid'];
+
+  $sql="delete from `course` where id=$id";
+
+
+  $result=mysqli_query($con,$sql);
+  if($result){
+      header('location:course.php');
+  }else{
+    die(mysqli_error($con));
+  }
+
+
+}
+
+
+
+?>
