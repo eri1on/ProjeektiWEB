b7f14f4442aeb706cd66ae8cc77944f4431433ff
eri1on <et58394@ubt-uni.net>
2023-02-24 12:01:46 +0100

    styled dashboard page

---
+++ DB/dashboard.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Dashboard</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+        <style>
+      /* Style for the header */
+      header {
+        background-color: #333;
+        color: #fff;
+        padding: 20px;
+        text-align: center;
+      }
+      /* Style for the buttons */
+      button {
+        background-color: #fff;
+        color: #333;
+        border: none;
+        padding: 10px 20px;
+        margin: 0 10px;
+        border-radius: 5px;
+        font-size: 16px;
+        font-weight: bold;
+        cursor: pointer;
+      }
+    </style>
+    </head>
+
+    <body>
+    <header>
+      <h1>Dashboard</h1>
+      <button> <a href ="../Course/course.php">Course   </a></button>
+      <button>Contact</button>
+    </header>
+    <main>
+      <p>This is the main content of the page.</p>
+    </main>
+        
+        <script src="" async defer></script>
+    </body>
+</html>
