8b79ae7d5665da17f4a4b24efa754815ba17c02a
eri1on <et58394@ubt-uni.net>
2023-02-25 19:25:37 +0100

    display from database table 'contact' every input into dashboard page

--- DB/dashboard.php
+++ DB/dashboard.php
@@ -1,3 +1,38 @@
+<?php
+    session_start();
+    include '../DB/connect.php';
+    
+    if(!isset($_SESSION['username'])){
+        header('location:login.php');
+    } else {
+        $username = $_SESSION['username'];
+    }
+
+
+
+    $query = "SELECT role FROM user WHERE username='$username'";
+    $result = mysqli_query($con, $query);
+    $row = mysqli_fetch_assoc($result);
+    $role = $row['role'];
+    
+   
+    if (!$role == 1) {
+       header('location:../home.php');
+    } 
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 
 <html>
@@ -7,38 +42,68 @@
         <title>Dashboard</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
-        <style>
-      /* Style for the header */
-      header {
-        background-color: #333;
-        color: #fff;
-        padding: 20px;
-        text-align: center;
-      }
-      /* Style for the buttons */
-      button {
-        background-color: #fff;
-        color: #333;
-        border: none;
-        padding: 10px 20px;
-        margin: 0 10px;
-        border-radius: 5px;
-        font-size: 16px;
-        font-weight: bold;
-        cursor: pointer;
-      }
-    </style>
+        <link rel="stylesheet" href="dashboard-style.css">
+    
     </head>
 
     <body>
     <header>
       <h1>Dashboard</h1>
       <button> <a href ="../Course/course.php">Course   </a></button>
-      <button>Contact</button>
+      <button> <a href="dashboard.php" > Contact </a></button>
+      <button> <a href="../home.php">Home</a></button>
     </header>
     <main>
-      <p>This is the main content of the page.</p>
+      <section class="contact-section">
+    <table>
+                <tr>
+                    <th>Name</th>
+                    <th>Lastname</th>
+                    <th>Email</th>
+                    <th>Telephone</th>
+                    <th class="message">Message</th>
+                    <th class="contact-id">Contact_Id</th>
+                </tr>
+                
+                <?php
+                    $sql = "SELECT * FROM `contact`";
+                    $result = mysqli_query($con, $sql);
+                    
+                    if ($result) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            $contact_id = $row['contact_id'];
+                            $name = $row['name'];
+                            $lastname = $row['lastname'];
+                            $email = $row['email'];
+                            $telephone=$row['telephone'];
+                            $message=$row['message'];
+                            '<tr>';
+                            echo '<td scope="row" class="contact-id">'.$contact_id.'</td>';
+                            echo '<td>'.$name.'</td>';
+                            echo '<td>'.$lastname.'</td>';
+                            echo '<td>'.$email.'</td>';
+                            echo '<td>'.$telephone.'<td>';
+                            echo '<td >'.$message.'<td>';
+                        
+                            
+                            // display the update and delete buttons if the user is an admin
+                            if ($role == 1) {
+                               
+                                echo '<td>  <button class="btn-delete"> <a href="delete-contact.php?deleteid='.$contact_id.'">Delete</a> </button>  </td>';
+                            }
+                            
+                            echo '</tr>';
+                        }
+                    }
+                ?>
+            </table>  
+
+                  </section>
+
+
+
+
+
     </main>
         
         <script src="" async defer></script>
