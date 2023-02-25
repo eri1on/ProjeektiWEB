03bb336fefe416ac49d1503ad189389d825cecbe
eri1on <et58394@ubt-uni.net>
2023-02-25 19:17:31 +0100

    phone and message field validation added

--- home-Validation.js
+++ home-Validation.js
@@ -4,14 +4,18 @@ function  Validation(){
 const NameRegex=/^[A-Za-z]+[a-zA-Z]{1,}/;
 const LastNameRegex=/^[A-Za-z]+[a-zA-Z]{1,}/;
 const EmailRegex=/^[a-z0-9]+([_.-][a-z0-9]+)*@[a-z0-9]+([.-][a-z0-9]+)*\.[a-z]{2,3}$/;
-
+const PhoneRegex=/^\d{9,10}$/;
 
 var NameInput=document.getElementById('name').value;
 var LastnameInput=document.getElementById('lastname').value;
 var EmailInput=document.getElementById('email').value;
+var PhoneInput=document.getElementById('telephone').value;
+var MessageInput=document.getElementById('message').value;
 
 
-if(NameInput===""){
+
+
+if(NameInput.trim()===""){
     alert("Please enter your name !")
     document.getElementById('name').focus();
     return false;
@@ -23,7 +27,8 @@ if(!NameRegex.test(NameInput)){
     return false;
 }
 
-if(LastnameInput===""){
+
+if(LastnameInput.trim()===""){
   alert("Please enter you lastname");
   document.getElementById('lastname').focus();
   return false;
@@ -31,12 +36,24 @@ if(LastnameInput===""){
 
 if(!LastNameRegex.test(LastnameInput)){
      alert("Lastname should only include letters, and have at least 2 characters");
-     document.getElementById('lastname').focus()
+     document.getElementById('lastname').focus();
      return false;
 }
 
 
-if(EmailInput===""){
+if(PhoneInput.trim()===""){
+    alert("Please enter you Phone Number !");
+    document.getElementById('telephone').focus();
+    return false;
+}
+if(!PhoneRegex.test(PhoneInput)){
+  alert("Phone number can contains only numbers and have 9 or 10 digits");
+  document.getElementById('telephone').focus();
+  return false;
+}
+
+
+if(EmailInput.trim()===""){
   alert("Email cannot be empty !");
   document.getElementById("email").focus();
   return false;
@@ -48,16 +65,13 @@ document.getElementById('email').focus();
 return false;
 }
    
-
-alert("validation completed successfully");
+if(MessageInput.trim()===""){
+  alert("Please enter a message !!");
+  document.getElementById("message").focus();
+  return false;
 
 }
-/*  it clears the field input  when submit button is pressed
 
-function  {
-    document.getElementById("name").value = "";
-    document.getElementById("lastname").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("textarea").value = "";
-  }
-*/
+alert("Message sent successfully");
+
+}
