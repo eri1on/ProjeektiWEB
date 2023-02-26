45013014abb2653fdbbd7ee1a42e71aeb93920c6
eri1on <et58394@ubt-uni.net>
2023-02-26 20:45:32 +0100

    show dashboard button in course.php only if user is admin

--- Course/course.php
+++ Course/course.php
@@ -7,6 +7,15 @@
     } else {
         $username = $_SESSION['username'];
     }
+
+    $query = "SELECT role FROM user WHERE username='$username'";
+            $result = mysqli_query($con, $query);
+            $row = mysqli_fetch_assoc($result);
+            $role = $row['role'];
+
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -22,26 +31,59 @@
 <body>
     <header>
         <h1>Our Courses</h1>
+        <button ><a href="../home.php" style="color:#40513B;">HOME</a></button>
+
+        <?php
+        
+            //display the Dashboard button if the user is an admin
+        if($role == 1){
+       echo ' <button ><a href="../DB/dashboard.php" style="color:#40513B;">Dashboard</a></button>';
+        }
+        ?>
     </header>
     
     <main>
         <section class="marketing">
-            <p class="p">Learn from the best! Our courses are designed to help you master new skills quickly and effectively. With our experienced instructors and hands-on projects, you'll gain practical experience and be ready to apply your new skills in the real world. Choose from a variety of courses below and start learning today!</p>
-        </section>
-        
-        <?php
-            // check if the user is an admin
-            $query = "SELECT role FROM user WHERE username='$username'";
-            $result = mysqli_query($con, $query);
-            $row = mysqli_fetch_assoc($result);
-            $role = $row['role'];
+          
+        <p class="p">Learn from the best! Our courses are designed to help you master new skills quickly and effectively. With our experienced instructors and hands-on projects, you'll gain practical experience and be ready to apply your new skills in the real world. Choose from a variety of courses below and start learning today!</p>
+        <section class="courses-container">
             
-            // display the add course button if the user is an admin
+  <div class="course">
+    <img class="img1"src="https://cdn-icons-png.flaticon.com/512/9833/9833682.png">
+    <h3>Save your Time</h3>
+    <p>Dont waste time looking for other Courses</p>
+  </div>
+  <div class="course">
+  <img class="img1" src="https://cdn-icons-png.flaticon.com/512/8991/8991885.png">
+  <h3>Skills</h3>
+    <p>Learn Skills that's worth it</p>
+  </div>
+  <div class="course">
+  <img class="img1"src="https://t4.ftcdn.net/jpg/03/46/06/79/240_F_346067974_0iyoKUZgeQ1zMmD0ClLOAWi2qpq8QAZG.jpg">
+  <h3>Everything in one Place</h3>
+    <p>Full stack Developer</p>
+  </div>
+ 
+</section>
+    </section>
+    
+       
+   
+    
+   <img  class="course-img"src="https://cdn-icons-png.flaticon.com/512/906/906175.png">
+   <h2 class="h2-why">Be Part Of Us    </h2>
+    
+
+<div>
+        <?php
+            
+            
+            //display the add course button if the user is an admin
             if ($role == 1) {
                 echo '<button class="btn-create"> <a href="create-course.php">ADD COURSE </a></button>';
             }
         ?>
-        
+        </div>
         <section class="courses">
             <table>
                 <tr>
@@ -73,7 +115,7 @@
                                 echo '<td>  <button class="btn-update"> <a href="update-course.php?updateid='.$id.'">Update</a> </button>  </td>';
                                 echo '<td>  <button class="btn-delete"> <a href="delete-course.php?deleteid='.$id.'">Delete</a> </button>  </td>';
                             }
-                            
+                            echo '<td>  <button class="enroll"> <a href="#">Enroll</a> </button>  </td>';
                             echo '</tr>';
                         }
                     }
